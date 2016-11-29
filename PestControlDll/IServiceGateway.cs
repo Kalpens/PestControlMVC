456caf4887c7825e4bc61f2456a2f179d2225bff
Kalpens <m.kalejs@outlook.com>
2016-11-29 11:34:54 +0100

    Comments

--- PestControlDll/IServiceGateway.cs
+++ PestControlDll/IServiceGateway.cs
@@ -8,10 +8,34 @@ namespace PestControlDll
 {
     public interface IServiceGateway<T>
     {
+        /// <summary>
+        /// Creates a new object of provided type in API
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
         T Put(T t);
+        /// <summary>
+        /// Gets one object from api who has the exact id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         T Get(int id);
+        /// <summary>
+        /// Gets a list of objects
+        /// </summary>
+        /// <returns></returns>
         List<T> Get();
+        /// <summary>
+        /// Updates provided object
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
         T Post(T t);
+        /// <summary>
+        /// Deletes provided object
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         bool Delete(int id);
     }
 }
