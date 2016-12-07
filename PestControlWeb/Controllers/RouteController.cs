5ec9a49778f26bb35eba46f647c28674eea12334
Drakzel <kris501i@easv365.dk>
2016-12-07 14:42:35 +0100

    Mareks, helpskidudel! -> RouteController

---
+++ PestControlWeb/Controllers/RouteController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PestControlWeb.Controllers
+{
+    public class RouteController : Controller
+    {
+        // GET: Route
+        public ActionResult Index()
+        {
+            //ViewBag. <--- This has no reference, but i need something like Viewbag.Route = List<Routes> 
+            return View();
+        }
+
+        // GET: Route/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Route/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Route/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Route/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Route/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Route/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Route/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
