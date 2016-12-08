c70d1561624a1028763ed1e2cd807872be86eb37
Kalpens <m.kalejs@outlook.com>
2016-12-08 21:00:29 +0100

    Maps display proper routes, also whenever a route is passed onto view, their destinations should be automatically added to the Map!!

--- PestControlWeb/Controllers/HomeController.cs
+++ PestControlWeb/Controllers/HomeController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PestControlDll.Entities;
 
 namespace PestControlWeb.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(Route route)
         {
-            return View();
+            return View(route);
+        }
+        [HttpPost]
+        public ActionResult GenerateMap()
+        {
+            return RedirectToAction("Index");
         }
     }
-}
+}   
