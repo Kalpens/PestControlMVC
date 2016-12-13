a23d2094c4c3e6ba75aaf0bfacb8ee5ce890c2b6
Kalpens <m.kalejs@outlook.com>
2016-12-13 19:40:45 +0100

    Waaaaaaaait

--- PestControlDll/Entities/Worksheet.cs
+++ PestControlDll/Entities/Worksheet.cs
@@ -19,6 +19,7 @@ namespace PestControlDll.Entities
         public string RekvNumber { get; set; }
         public int Price { get; set; }
         public string Note { get; set; }
+        public Boolean ReVisited { get; set; }
         public List<PestType> PestTypes { get; set; }
 
         //Enums associated its value with ordered numbers, starting from 0.
